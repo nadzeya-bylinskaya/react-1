0d7237c530f40c9f88b528bdbe7fcbc14596b026
sgurin <work.gurin@gmail.com>
2019-06-12 10:07:17 +0300

    feat: changed MyPosts

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,6 +5,13 @@ import Post from "./Post/Post";
 const MyPosts = (props) => {
 
     let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount} />)
+
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert (text)
+    };
   return (
          <div className={s.postsBlock}>
       <div>
@@ -12,9 +19,9 @@ const MyPosts = (props) => {
       </div>
       <div>
         <div>
-          <textarea />
+            <textarea ref={newPostElement}></textarea>
         </div>
-        <button>Add post</button>
+        <button onClick={ addPost }>Add post</button>
       </div>
       <div className={s.posts}>
           {postsElements}
