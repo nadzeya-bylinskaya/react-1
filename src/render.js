0a89b8584d7d36716f8c67858b73f98c652cd8f1
sgurin <work.gurin@gmail.com>
2019-08-07 01:53:02 +0300

    addPost FLUX

---
+++ src/render.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import {addMessage} from "./redux/state"
+import {addPost} from "./redux/state"
+import {BrowserRouter} from "react-router-dom";
+
+export let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state} addPost={addPost} addMessage={addMessage}/>
+        </BrowserRouter>, document.getElementById("root"));
+
+
+};
+
+
