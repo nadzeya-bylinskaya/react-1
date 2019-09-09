015ca01f500b3c3b57200fc5876ce8725edc46da
Nadzeya <nadzeya-bylinskaya@gmail.com>
2019-09-09 21:52:07 +0300

    class component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,6 +16,11 @@ const Navbar = props => {
           Messages
         </NavLink>
       </div>
+      <div className={`${s.item} ${s.active}`}>
+        <NavLink to="/users" activeClassName={s.activeLink}>
+          Users
+        </NavLink>
+      </div>
       <div className={s.item}>
         <NavLink to="/news" activeClassName={s.activeLink}>
           News
