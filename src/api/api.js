dfeff781f313dd0d966943f4ee017c43cf951369
Nadzeya <nadzeya-bylinskaya@gmail.com>
2019-10-02 03:18:29 +0300

    DAL, axios.create

---
+++ src/api/api.js
@@ -0,0 +1,21 @@
+import * as axios from "axios";
+
+const baseUrl = `https://social-network.samuraijs.com/api/1.0/`;
+
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: "https://social-network.samuraijs.com/api/1.0/",
+  headers: {
+    "API-KEY": "75cb9032-d984-4e3c-881f-34406d18d9e9"
+  }
+});
+
+export const usersAPI = {
+  getUsers(currentPage = 1, pageSize = 10) {
+    return instance
+      .get(`users?page=${currentPage}&count=${pageSize}`)
+      .then(response => {
+        return response.data;
+      });
+  }
+};
